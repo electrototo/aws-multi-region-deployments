4c643753f1d2c87047a6d371641f91dca80f2d72
electrototo <cristobal@liendo.net>
2024-10-09 20:55:18 -0700

    Change certificate validation

--- lib/serverless-api-stack.ts
+++ lib/serverless-api-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 
@@ -19,7 +19,8 @@ export class ServerlessAPIStack extends cdk.Stack {
 
     const regionalizedDomainName = `${this.region}.c.liendo.net`;
     const acmCertificate = new Certificate(this, `${this.region}-RegionalizedACM`, {
-      domainName: regionalizedDomainName
+      domainName: regionalizedDomainName,
+      validation: CertificateValidation.fromDns()
     });
 
     apiGateway.addDomainName(`${this.region}-DomainName`, {
